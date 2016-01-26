0c67f7f6317bb0d0b1d5cd029f70ed2c84961c43
Andrew Dixon <dixon@atomicbroadcast.net>
2016-01-26 14:48:23 -0800

    Only Inject Mixpanel into TrackingService

--- app/initializers/mixpanel.js
+++ app/initializers/mixpanel.js
@@ -1,9 +1,6 @@
 export function initialize() {
   let application = arguments[1] || arguments[0];
-  application.inject('route', 'mixpanel', 'service:mixpanel');
-  application.inject('router:main', 'mixpanel', 'service:mixpanel');
-  application.inject('controller', 'mixpanel', 'service:mixpanel');
-  application.inject('component', 'mixpanel', 'service:mixpanel');
+  application.inject('service:tracking', 'mixpanel', 'service:mixpanel');
 }
 
 export default {
